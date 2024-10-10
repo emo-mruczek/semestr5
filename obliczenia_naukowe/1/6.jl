900cfc4721fcde713c2ebc892a62ced02ebf8849
emo-mruczek <krokcia1@gmail.com>
2024-10-11 00:07:23 +0200

    on 1: add 6

--- obliczenia_naukowe/1/6.jl
+++ obliczenia_naukowe/1/6.jl
@@ -1,4 +1,5 @@
 #TODO: be consistent in one(T) throughout ex
+#TODO: check the results
 
 function f(x, T)
     return sqrt(x^2 + one(T)) - one(T)
@@ -8,7 +9,9 @@ function g(x, T)
     return x^2 / ( sqrt(x ^ one(T)) + one(T) )
 end
 
-x = 8
-for p in 1:5
-    prinln("f(", x, "^-", p, "): ", f(x^(-p)))
+x = 8.0
+for p in 1:10
+    println("f(", x, "^-", p, "): ", f(x^(-p), Float64))
+    println("g(", x, "^-", p, "): ", g(x^(-p), Float64))
+    println("\n")
 end
