48c9224aa33e8fd12ba67f43bfdf64e57d431923
emo-mruczek <krokcia1@gmail.com>
2024-10-11 13:08:08 +0200

    aod 1: some shitty code do not look plz

--- algorytmy_optymalizacji_dyskretnej/1/3/main.c
+++ algorytmy_optymalizacji_dyskretnej/1/3/main.c
@@ -1,3 +1,6 @@
+//TODO: refactor
+//TODO: free
+//TODO: should print scc
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +9,11 @@
 void DFS(void);
 void DFS_visit(uint32_t* visited_in_order, uint32_t* iter, uint8_t* visited, uint32_t vertex);  
 void add_edge(uint32_t v, uint32_t u);
+void add_edge_T(uint32_t v, uint32_t u);
+void transpose(void);
+//temp
+void DFS_T(void);
+uint32_t* DFS_visit_T(uint32_t* visited_in_order, uint32_t* iter, uint8_t* visited, uint32_t vertex);
 
 volatile uint8_t is_directed;
 volatile uint32_t num_of_vertices;
@@ -18,6 +26,11 @@ typedef struct node {
 } node;
 
 struct node** G;
+struct node** T;
+
+// temppp
+uint32_t* visited_in_order; 
+
 
 int main(int argc, char** argv) {
     if ( debug ) printf("(D)irected or (U)ndirected?\n");
@@ -54,19 +67,38 @@ int main(int argc, char** argv) {
         } 
     }
 
+    visited_in_order = (uint32_t*)malloc(num_of_vertices * sizeof(uint32_t));
+    memset(visited_in_order, 0, (num_of_vertices) * sizeof(uint32_t));
+
+
     DFS();
+
+    transpose();
+
+    if ( debug ) {
+        for ( uint32_t i = 1; i < num_of_vertices + 1; ++i ) {
+            node* temp = T[i];
+            printf("\n %" PRIu32 "\n\t", i);
+            while ( temp ) {
+                printf("%" PRIu32 " ", temp->vertex);
+                temp = temp->next;
+            }
+        } 
+    }
+
+    DFS_T();
+
+   
 
     //TODO: free
     for ( uint32_t i = 0; i < num_of_vertices + 1; ++i ) {
         free(G[i]);
     }
     free(G);
+    free(visited_in_order);
 }
 
 void DFS(void) {
-    printf("\nDFS:\n");
-    uint32_t* visited_in_order = (uint32_t*)malloc(num_of_vertices * sizeof(uint32_t));
-    memset(visited_in_order, 0, (num_of_vertices) * sizeof(uint32_t));
     uint32_t iter = 0;
     uint8_t* visited = (uint8_t*)malloc((num_of_vertices + 1) * sizeof(uint8_t));
     memset(visited, 0, (num_of_vertices + 1) * sizeof(uint8_t));
@@ -79,12 +111,11 @@ void DFS(void) {
     }
     /* algorithm ends */
 
-    printf("\n visited in order:\n");
+    printf("\nvisited in order:\n");
 
     for ( uint32_t i = 0; i < num_of_vertices; ++i ) printf("%" PRIu32 " ", visited_in_order[i]);
 
     free(visited);
-    free(visited_in_order);
 }
 
 void DFS_visit(uint32_t* visited_in_order, uint32_t* iter, uint8_t* visited, uint32_t vertex) {
@@ -94,7 +125,6 @@ void DFS_visit(uint32_t* visited_in_order, uint32_t* iter, uint8_t* visited, uin
     node* temp = G[vertex];
     while ( temp ) {
         if ( !visited[temp->vertex] ) DFS_visit(visited_in_order, iter, visited, temp->vertex);
-        //++temp;
         temp = temp->next;
     }
 
@@ -103,13 +133,74 @@ void DFS_visit(uint32_t* visited_in_order, uint32_t* iter, uint8_t* visited, uin
     /* algorithms ends */
 }
 
-/*void transpose(void) {
-    for ( uint32_t i = 0; i <= num_of_vertices; ++i ) {
-        for ( uint32_t j = 0; j <= num_of_vertices; ++j ) {
-            if ( G[i][j] )
+void transpose(void) {
+    T = (node**)malloc((num_of_vertices + 1) * sizeof(node*));
+
+    for ( uint32_t i = 1; i <= num_of_vertices; ++i ) {
+        node *temp = G[i];
+        while ( temp ) {
+            add_edge_T(temp->vertex, i); 
+            temp = temp->next;
         }
     }
-}*/
+}
+
+/* for transposed */
+
+void DFS_T(void) {
+    uint32_t* component_order = (uint32_t*)malloc((num_of_vertices) * sizeof(uint32_t));
+    memset(component_order, 0, (num_of_vertices) * sizeof(uint32_t));
+    uint8_t* visited = (uint8_t*)malloc((num_of_vertices + 1) * sizeof(uint8_t));
+    memset(visited, 0, (num_of_vertices + 1) * sizeof(uint8_t));
+    uint32_t iter = 0;
+    uint32_t prev = 0;
+    uint32_t* to_print;
+    uint32_t number = 0;
+
+    /* algorithm starts */
+    for ( uint32_t i = 0; i < num_of_vertices; ++i ) {
+        if ( !visited[i] ) {
+            to_print = DFS_visit_T(component_order, &iter, visited, visited_in_order[num_of_vertices - i]);
+        }
+        ++number;
+        printf("\nNo of scc: %" PRIu32 "\n", number);
+        for ( uint32_t i = prev; i < iter; ++i ) printf("%" PRIu32 " ", to_print[i]);
+        prev = iter;
+    }
+    /* algorithm ends */
+
+    printf("\nvisited in order:\n");
+
+    for ( uint32_t i = 0; i < num_of_vertices; ++i ) printf("%" PRIu32 " ", component_order[i]);
+
+    printf("\nNum of componens %" PRIu32, number);
+
+    free(visited);
+}
+
+uint32_t* DFS_visit_T(uint32_t* visited_in_order, uint32_t* iter, uint8_t* visited, uint32_t vertex) {
+    /* algorithm starts */
+    visited[vertex] = 1;
+
+    node* temp = T[vertex];
+    while ( temp ) {
+        if ( !visited[temp->vertex] ) DFS_visit_T(visited_in_order, iter, visited, temp->vertex);
+        temp = temp->next;
+    }
+
+    visited_in_order[*iter] = vertex;
+    ++(*iter);
+
+    return visited_in_order;
+    /* algorithms ends */
+}
+
+void add_edge_T(uint32_t v, uint32_t u) {
+    node* new = (node*)malloc(sizeof(node));
+    new->vertex = u;
+    new->next = T[v];
+    T[v] = new;
+}
 
 void add_edge(uint32_t v, uint32_t u) {
     node* new = (node*)malloc(sizeof(node));
