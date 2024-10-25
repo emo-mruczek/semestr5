7f072ab27a62ba97494afc4eac6f7a89f4ad5478
emo-mruczek <krokcia1@gmail.com>
2024-10-25 16:02:19 +0200

    on 1: spr init

--- obliczenia_naukowe/1/3.jl
+++ obliczenia_naukowe/1/3.jl
@@ -1,33 +1,17 @@
-using Base.Threads
-
 # bitstring(n) A string giving the literal bit representation of a primitive type. 
+
 function is_evenly(step, b::Float64, e::Float64)
+
     x = b
-    dif = Float64[]
+    y = b
 
-    while x + step < e
-        next = x + step
-        push!(dif, next - x)
-        x = next
-        println(x, " ")
+    while x < e
+        println(bitstring(x))
+        println(bitstring(y))
+        x += step
+        y = nextfloat(y)
     end
-
-    return all(d -> d == step, dif)
 end
 
-function is_evenly_threads(step, b::Float64, e::Float64)
-    threads = nthreads()
-    range = ( e - b ) / threads
-    results = Vector{Bool}(undef, threads)
+println(is_evenly(2.0^(-52), 1.0, 2.0))
 
-    @threads for i in 1:threads
-        start = b + ( i - 1 ) * range
-        stop = ( i == threads ? e : start + range )
-        results[i] = is_evenly(step, start, stop)
-    end
-
-    return all(results)
-end
-
-
-println("Are they equal? ", is_evenly_threads(2.0^(-52), 1.0, 2.0))
