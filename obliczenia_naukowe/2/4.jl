75cbc21e9be6880f3de65956debb19f6d8563889
emo-mruczek <krokcia1@gmail.com>
2024-11-08 21:43:24 +0100

    on 2: 4 complex

--- obliczenia_naukowe/2/4.jl
+++ obliczenia_naukowe/2/4.jl
@@ -40,7 +40,7 @@ end
 
 # slight change in coefficients in order to reproduce Wilkinson's experiment
 
-wil_coef::Vector{Float64} = [1, -210.0-2.0^(-23.0), 20615.0,-1256850.0,
+wil_coef::Vector{Float64} = [1, -210.0-(2)^(-23), 20615.0,-1256850.0,
                         53327946.0,-1672280820.0, 40171771630.0, -756111184500.0,          
                         11310276995381.0, -135585182899530.0,
                         1307535010540395.0,     -10142299865511450.0,
@@ -56,9 +56,9 @@ println("Polynomial: \n")
 println(my_poly_b)
 
 # using roots() in order to calculate, well, roots
-my_poly_roots_b::Vector{Float64} = roots(my_poly_b)
+my_poly_roots_b::Vector{ComplexF64} = roots(my_poly_b)
 #println(my_poly_roots)
-#
+
 # wilkinsons polynomial using fromroots
 wilkinsons_poly_b::Polynomial = fromroots(my_poly_roots_b)
 println(wilkinsons_poly_b, "\n")
@@ -66,7 +66,7 @@ println(wilkinsons_poly_b, "\n")
 println("Results for polynomial b: k --- z_k --- Pz_k --- pz_k --- diff\n")
 
 for i::Int64 in 1:20
-    z_k::Float64 = my_poly_roots_b[i]
+    z_k::ComplexF64 = my_poly_roots_b[i]
     Pz_k::Float64 = abs(my_poly_b(z_k))
     pz_k::Float64 = abs(wilkinsons_poly_b(z_k))
     diff::Float64 = abs(z_k - i)
