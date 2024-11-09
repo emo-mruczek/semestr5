844e7ac04cf2e05f6a3f42bd27577a5ba2c3d18e
emo-mruczek <krokcia1@gmail.com>
2024-11-10 00:30:18 +0100

    on fix!!!!!!!!! thanks poz!!!!

--- obliczenia_naukowe/2/5.jl
+++ obliczenia_naukowe/2/5.jl
@@ -7,10 +7,10 @@ end
 # no idea how to make it T
 # its so ugly do not look at it
 
-function experiment_1a_32()::Vector{Float32}
-    res::Vector{Float32} = Vector{Float32}()
-    p::Float32 = 0.01
-    r::Float32 = 3.0
+function experiment_1a(T::Type{<: AbstractFloat})::Vector{T}
+    res::Vector{T} = Vector{T}()
+    p::T = 0.01
+    r::T = 3.0
     push!(res, p)
 
     for _ in 1:40
@@ -20,21 +20,6 @@ function experiment_1a_32()::Vector{Float32}
     
     return res
 end
-
-function experiment_1a_64()::Vector{Float64}
-    res::Vector{Float64} = Vector{Float64}()
-    p::Float64 = 0.01
-    r::Float64 = 3.0
-    push!(res, p)
-
-    for _ in 1:40
-        p = model(r, p)
-        push!(res, p)
-    end
-    
-    return res
-end
-
 
 function experiment_1b()::Vector{Float32}
     res::Vector{Float32} = Vector{Float32}()
@@ -59,9 +44,9 @@ function experiment_1b()::Vector{Float32}
 end
 
 
-p_1::Vector{Float32} = experiment_1a_32()
+p_1::Vector{Float32} = experiment_1a(Float32)
 p_2::Vector{Float32} = experiment_1b()
-p_3::Vector{Float64} = experiment_1a_64()
+p_3::Vector{Float64} = experiment_1a(Float64)
 
 println("\nResults: n --- p_1 --- p_2 --- p_3\n")
 for n in 1:41
