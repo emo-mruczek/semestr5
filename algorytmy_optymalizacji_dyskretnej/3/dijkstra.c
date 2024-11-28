c7fc7d49fb9c10aa3a0aead7c6c6264082c6ae02
emo-mruczek <krokcia1@gmail.com>
2024-11-28 18:56:12 +0100

    aod 3: input

--- algorytmy_optymalizacji_dyskretnej/3/dijkstra.c
+++ algorytmy_optymalizacji_dyskretnej/3/dijkstra.c
@@ -1,4 +1,5 @@
 // TODO:  stack limits
+// TODO: error handling?
 #include "sys/types.h"
 #include <stdio.h>
 #include <inttypes.h>
@@ -16,21 +17,115 @@ typedef struct Node {
     uint64_t w; // w(node, next)    
 } Node;
 
-typedef struct Graph {
-    uint64_t num_of_vertices;
-    uint64_t num_of_edges;
-    Node** G;
-} Graph;
+static uint64_t num_of_vertices;
+static uint64_t num_of_edges;
+static Node** G;
 
+static const char* data = NULL;
+static const char* in = NULL;
+static const char* res = NULL;
+
+bool get_args(int argc, char* argv[]);
+void process_input(const char* filename);
+void add_edge(uint64_t v, uint64_t u, uint64_t c,  Node** Graph);
+void print_graph(Node** Graph);
+void cleanup(void);
 
 int main(int argc, char* argv[]) {
+    //TODO: graph allocation
+
+    if (!(get_args(argc, argv))) exit(EXIT_FAILURE);
+    process_input(data);
+
+    if (debug) {
+        printf("\nInput graph:\n");
+        print_graph(G);
+        printf("\n");
+    }
+
+    cleanup();
+
+    exit(EXIT_SUCCESS);
+}
+
+void cleanup(void) {
+    for (uint64_t i = 1; i <= num_of_vertices; ++i) {
+        Node* current = G[i];
+
+        while (current) {
+            Node* next = current->next;
+            free(current);
+            current = next;
+        }
+    }
+
+    free(G);
+}
+
+void print_graph(Node** Graph) {
+    for ( uint64_t i = 1; i < num_of_vertices + 1; ++i ) {
+        Node* temp = Graph[i];
+        printf("\n %" PRIu64 "\n\t", i);
+
+        while ( temp ) {
+            printf("(%" PRIu64 ", %" PRIu64 ") ", temp->vertex, temp->w);
+            temp = temp->next;
+        }
+    }
+
+    printf("\n");
+}
+
+void add_edge(uint64_t v, uint64_t u, uint64_t c,  Node** Graph) {
+    Node* new = (Node*)malloc(sizeof(Node));
+    new->vertex = u;
+    new->next = Graph[v];
+    new->w = c;
+    Graph[v] = new;
+}
+
+void process_input(const char* filename) {
+    FILE* file = fopen(filename, "r");
+
+    if (!file) exit(EXIT_FAILURE);
+
+    // TODO: size?
+    char buf[256];
+
+    // TODO: validation?
+
+    while (fgets(buf, sizeof buf, file) != NULL) {
+        if (debug) printf("%s\n", buf);
+        char b = buf[0];
+        uint64_t src = 0;
+        uint64_t dst = 0;
+        uint64_t cst = 0;
+
+        switch (b) {
+            case 'c':
+                continue;
+
+            case 'p':
+                //TODO: eeee
+                sscanf(buf, "p sp %" SCNu64 " %" SCNu64, &num_of_vertices, &num_of_edges);
+                G = (Node**)malloc((num_of_vertices + 1) * sizeof(Node*));
+                for (uint32_t i = 1; i <= num_of_vertices; ++i) G[i] = NULL;
+                break;
+
+            case 'a':
+                sscanf(buf, "a %" SCNu64 " %" SCNu64 " %" SCNu64, &src, &dst, &cst);
+                add_edge(src, dst, cst, G);
+                break;
+        }
+    }
+ 
+    if (feof(file)) if (debug) printf("End of file reached\n");
+}
+
+bool get_args(int argc, char* argv[]) {
 
     // TODO: more checks
     // TODO: stderr?
-    
-    const char* data = NULL;
-    const char* in = NULL;
-    const char* res = NULL;
 
     bool is_data = false;
     bool is_in = false;
@@ -48,30 +143,29 @@ int main(int argc, char* argv[]) {
     int c;
     while (( c = getopt_long_only(argc, argv, "s:o:p:r:d:", long_options, NULL)) != -1) {
       
-
         switch (c) {
             case 's':
-                printf("Option ss with value '%s'\n", optarg);
+                if (debug) printf("Option ss with value '%s'\n", optarg);
                 in = optarg;
                 is_in = true;
                 break;
             case 'o':
-                printf("Option oss with value '%s'\n", optarg);
+                if (debug) printf("Option oss with value '%s'\n", optarg);
                 res = optarg;
                 is_res = true;
                 break;
             case 'p':
-                printf("Option p2p with value '%s'\n", optarg);
+                if (debug) printf("Option p2p with value '%s'\n", optarg);
                 in = optarg;
                 is_in = true;
                 break;
             case 'r':
-                printf("Option op2p with value '%s'\n", optarg);
+                if (debug) printf("Option op2p with value '%s'\n", optarg);
                 res = optarg;
                 is_res = true;
                 break;
             case 'd':
-                printf("Option d with value '%s'\n", optarg);
+                if (debug) printf("Option d with value '%s'\n", optarg);
                 data = optarg;
                 is_data = true;
                 break;
@@ -93,8 +187,8 @@ int main(int argc, char* argv[]) {
 
     if (!(is_res && is_in && is_data)) {
         printf("Not enough arguments!\n");
-        return(EXIT_FAILURE);
+        return false;
     }
 
-    exit(EXIT_SUCCESS);
+    return true;
 }
